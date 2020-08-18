66fd2abe2cb273285d2b56fa67e0e07d052ebcb7
Jarod Brennfleck <robinjars@gmail.com>
2020-08-18 10:14:04 +1000

    The big kahuna is ready...

---
+++ bigkahuna.js
@@ -0,0 +1,128 @@
+// TODO: integrate dotenv?
+
+class BigKahuna {
+    constructor(args) {
+        this.weight = this.length = this.size = 0;
+        this.args = {};
+        this.nodashArgs = {};
+
+        args = args || process.argv.slice(2);
+
+        this.processArgs(args);
+
+        this.careForDashes = false;
+
+        // We don't want to inf-loop when we make Strict Kahunas
+        if (this.constructor.name === 'BigKahuna') this.must = new StrictKahuna(args, this);
+    }
+
+    processArgs(args) {
+        this.args._ = (this.args._ || []).concat(args);
+        this.nodashArgs._ = (this.nodashArgs._ || []).concat(args.map(a => a.replace(/^-+/gm, '')));
+
+        for (let i = 0; i < args.length; i++) {
+            let a = args[i];
+            let p = null;
+
+            if (a.startsWith('-') && i + 1 < args.length) {
+                p = args[i + 1];
+                if (p.startsWith('-')) p = null;
+                else i++;
+            }
+
+            this.args[a] = p;
+            this.nodashArgs[a.replace(/^-+/gm, '')] = p;
+        }
+
+        this.weight = this.length = this.size = args.length;
+    }
+
+    dashCare(care) {
+        if (typeof care === 'undefined') care = !this.careForDashes;
+        this.careForDashes = care;
+        return this;
+    }
+
+    arg(item) {
+        return this.cabinet(item);
+    }
+    cabinet(item) {
+        let args = this.nodashArgs._;
+        if (this.careForDashes) args = this.args._;
+
+        if (typeof item === 'undefined') return args;
+
+        if (item >= args.length || item < 0) return undefined;
+        else return args[item];
+    }
+    folders() {
+        if (this.careForDashes) return this.args;
+        return this.nodashArgs;
+    }
+
+    has() {
+        let arg = Array.from(arguments) || [];
+        if (!arg) return false;
+        if (!this.careForDashes) arg = arg.map(a => a.replace(/^-+/gm, ''));
+
+        let args = this.cabinet();
+
+        for (const a of arg) {
+            if (args.includes(a)) return true;
+        }
+
+        return false;
+    }
+
+    answer() {
+        let arg = Array.from(arguments) || [];
+        if (!arg) return false;
+        if (!this.careForDashes) arg = arg.map(a => a.replace(/^-+/gm, ''));
+
+        let args = this.folders();
+
+        for (const a of arg) {
+            if (args.hasOwnProperty(a)) return args[a];
+        }
+        return undefined;
+    }
+}
+class StrictKahuna extends BigKahuna {
+    arg(item) {
+        return this.cabinet(item);
+    }
+    cabinet(item) {
+        let c = super.cabinet(item);
+        if (typeof c === 'undefined') throw new MissingRequiredArgumentException(item);
+        return c;
+    }
+    has() {
+        let doesHave = super.has.apply(this, Array.from(arguments));
+        if (!doesHave) throw new MissingRequiredArgumentException(Array.from(arguments));
+        return doesHave;
+    }
+    answer() {
+        let a = super.answer.apply(this, Array.from(arguments));
+        if (typeof a === 'undefined') throw new MissingRequiredArgumentException(Array.from(arguments));
+        return a;
+    }
+}
+class MissingRequiredArgumentException {
+    constructor(args) {
+        this.name = "MissingRequiredArgumentException";
+        this.message = genMessage(args);
+    }
+    genMessage(args) {
+        let randLol = [
+            'The Kahuna needed that paperwork yesterday!',
+            'This\'ll be chonked from your pay!',
+            'The whole world is stopping because of this!',
+            'You\' lunch break already finished, so you can\'t slack off now!'
+        ];
+        randLol = randLol[Math.floor(Math.random() * randLol.length)];
+
+        return `Required argument [${args}] was not provided! ` + randLol;
+    }
+}
+
+module.exports = new BigKahuna();
---
+++ readme.md
@@ -0,0 +1,83 @@
+<div style="text-align:center;"><img src="./img/logo.png" alt="big kahuna's tie" height="500"/></div>
+
+big kahuna does the thing that you want it to do
+
+it does the whole "process my args thing"
+
+because doing the whole "process my args thing" can get tedious
+
+i mean, look at this code:
+
+```javascript
+for(let i = 0; i < process.argv.length; i++) {
+    if(["-h", "--help", "-?", "--plz-help-me"].includes(process.argv[i])) printHelp();
+}
+
+let time = process.argv.indexOf("--whats-the-time-mr-wolf");
+let dinner = process.argv[time + 1]; // but what about the error case?!
+let run = process.argv[process.argv.indexOf("--required-runner") + 1]; // this is required!
+```
+
+yuck.
+
+not nice.
+
+what does the king fish have to say about it?
+
+```javascript
+let bossman = require("big-kahuna"); // or require("big-kahuna")(modify(process.argv));
+
+if(bossman.has("h", "help", "?", "plz-help-me")) printHelp();
+
+let dinnerTime = bossman.answer("time", "whats-the-time-mr-wolf", "the-time-plz");
+let run = bossman.must.answer("required-runner"); // .must is a strict kahuna!
+```
+
+ooooohh yeeaaaahh
+
+the top brass seems happy about that
+
+oh and dashes are optional. the kahuna strips them when asked for them
+
+"hang on... my devapp NEEDS dashes!"
+
+dw. the head honcho has got you
+
+```javascript
+let bossman = require("big-kahuna").dashCare(); // dashCare is a toggle flag!
+console.log(bossman.has("h"));   // false
+console.log(bossman.has("-h"));  // true
+console.log(bossman.has("--h")); // false
+
+// or dashCare can set the flag
+console.log(bossman.dashCare(false).has("h")); // true
+```
+
+got positional arguments? the big cheese can help you with that one
+
+```javascript
+let bossman = require("big-kahuna"); // dashCare is a toggle flag!
+
+for(let i = 0; i < bossman.weight; i++) { // bossman.size and bossman.length work too - they're aliases!
+    console.log(bossman.cabinet(i)); // also use arg(i) - just another alias to fit the theme!
+}
+// he likes named folders too: bossman.folders() will return the args as an object!
+```
+
+the chiefton can also handle late submissions (like your homework)
+
+```javascript
+let bossman = require("big-kahuna");
+
+bossman.processArgs(lateArgsThatCameLate); // adds it to the args already tracked - will blindly override!
+```
+
+the fat cat doesn't like having contractors who slow things down
+
+so he's willing to do all the work all alone!
+
+**he'll tell you what's up, so you can dictate what's going down!**
+
+<div style="text-align:center;">
+<img src="./img/logo-small.png" alt="big kahuna.js" width="150"/>
+</div>
